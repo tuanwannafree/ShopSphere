831c4c3cc290195f5ba465060609a4dca528025d
Anwishta <anwishtaghosh@gmail.com>
2024-11-09 22:32:11 +0530

    user management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; 
+import { Provider } from "react-redux";
 import store from "./pages/redux/store.js";
+//Auth
+import Login from "./pages/Auth/Login.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-
     <RouterProvider router={router} />
   </Provider>
 );
