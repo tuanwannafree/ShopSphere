28a9579fd06100facb0ed5c806cabd2791d3e673
Anwishta <anwishtaghosh@gmail.com>
2024-11-19 19:01:02 +0530

    store mern

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { Route, RouterProvider, Navigate, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./pages/redux/store.js";
-//Auth
-import Login from "./pages/Auth/Login.jsx";
 
+// Auth
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/login" element={<Login />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
     </Route>
   )
 );
