7a8af9327b0c71b8770ce86fc7714f927f517506
Anwishta <anwishtaghosh@gmail.com>
2024-11-30 22:36:14 +0530

    product management

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import connectDB  from './config/db.js'
 import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -24,5 +25,6 @@ app.use(cookieParser())
 app.use('/api/users', userRoutes);
 app.use('/api/category',categoryRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/upload', uploadRoutes)
 
 app.listen(port, ()=>{ console.log(`Server running on port ${port}`)})
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,8 +3,34 @@ import formidable from "express-formidable";
 const router = express.Router();
 import { authenticate, authorizeAdmin } from "../middleware/authMiddleware.js";
 import checkId from "../middleware/checkId.js";
-import { addProduct} from "../controllers/productController.js";
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductsById,
+  fetchAllProducts,
+  createProductReview,
+  fetchTopProducts,
+  fetchNewProducts
+} from "../controllers/productController.js";
 
-router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route("/allproducts").get(fetchAllProducts);
+router
+  .route("/:id/reviews")
+  .post(authenticate, authorizeAdmin,checkId ,createProductReview);
+
+router.get('/top', fetchTopProducts)
+router.get('/new', fetchNewProducts)
+router
+  .route("/:id")
+  .get(fetchProductsById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, removeProduct);
 
 export default router;
